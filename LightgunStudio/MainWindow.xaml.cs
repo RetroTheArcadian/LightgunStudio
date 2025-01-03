8829693174d99ddbd1f0d082bad784d309d03eaa
RetroTheArcadian <retro.the.arcadian@gmail.com>
2025-01-04 00:23:58 +0100

    use wpf

--- LightgunStudio/MainWindow.xaml.cs
+++ LightgunStudio/MainWindow.xaml.cs
@@ -1,89 +1,28 @@
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Runtime.InteropServices.WindowsRuntime;
-using LighgunStudioCore;
-using LighgunStudioCore.RawInput;
-using Microsoft.UI;
-using Microsoft.UI.Windowing;
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
 namespace LightgunStudio
 {
     /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public sealed partial class MainWindow : Window
+    public partial class MainWindow : Window
     {
-        public List<RawInputController> _Controllers = new List<RawInputController>();
-        private AppWindow _apw;
-        private OverlappedPresenter _presenter;
-
         public MainWindow()
         {
-            this.InitializeComponent();
-            GetAppWindowAndPresenter();
-            _apw.IsShownInSwitchers = false;
-            _presenter.SetBorderAndTitleBar(false, false);
-            GetRawInputDevices();
+            InitializeComponent();
         }
-
-        private void myButton_Click(object sender, RoutedEventArgs e)
+        private void CloseApp_Click(object sender, RoutedEventArgs e)
         {
-            myButton.Content = "Clicked";
-        }
-        /// <summary>
-        /// Enumerates the Raw Input Devices and places their corresponding RawInputDevice structures into a List<string>
-        /// We just filter to remove Keyboard devices and keep Mouse + HID devices
-        /// </summary>
-        private void GetRawInputDevices()
-        {
-            uint deviceCount = 0;
-            var dwSize = (Marshal.SizeOf(typeof(RawInputDeviceList)));
-
-            if (Win32API.GetRawInputDeviceList(IntPtr.Zero, ref deviceCount, (uint)dwSize) == 0)
-            {
-                var pRawInputDeviceList = Marshal.AllocHGlobal((int)(dwSize * deviceCount));
-                Win32API.GetRawInputDeviceList(pRawInputDeviceList, ref deviceCount, (uint)dwSize);
-
-                for (var i = 0; i < deviceCount; i++)
-                {
-                    // On Window 8 64bit when compiling against .Net > 3.5 using .ToInt32 you will generate an arithmetic overflow. Leave as it is for 32bit/64bit applications
-                    RawInputDeviceList rid = (RawInputDeviceList)Marshal.PtrToStructure(new IntPtr((pRawInputDeviceList.ToInt64() + (dwSize * i))), typeof(RawInputDeviceList));
-
-                    RawInputController c = new RawInputController(rid.hDevice, rid.dwType);
-
-                    if (c.DeviceType == RawInputDeviceType.RIM_TYPEHID || c.DeviceType == RawInputDeviceType.RIM_TYPEMOUSE)
-                    {
-                        _Controllers.Add(c);
-                    }
-                }
-                Marshal.FreeHGlobal(pRawInputDeviceList);
-                return;
-            }
-            throw new Win32Exception(Marshal.GetLastWin32Error());
-        }
-
-        public void GetAppWindowAndPresenter()
-        {
-            var hWnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
-            WindowId myWndId = Win32Interop.GetWindowIdFromWindow(hWnd);
-            _apw = AppWindow.GetFromWindowId(myWndId);
-            _presenter = _apw.Presenter as OverlappedPresenter;
+            Close();
         }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # LightgunStudio
-Setup and manage your lightgun and games
+
+Setup and manage your lightgun and games.
+
+## Features
+
+- Setup multiple combinations of your lightguns
+- Install dependencies in one click
+- Configures [EmulationStation Desktop Edition](https://www.es-de.org/)
+- Creates structure for your games
+- Creates AutoHotKey scripts for launching games
+- Generates xml settings for demulshooter, TeknoParrot etc.
+- Allows easy switching between your defined lightgun combinations
+
+## Table of contents
+
+- [LightgunStudio](#lightgunstudio)
+  - [Features](#features)
+  - [Table of contents](#table-of-contents)
+  - [Quick start](#quick-start)
+  - [Bugs and feature requests](#bugs-and-feature-requests)
+  - [Contributing](#contributing)
+  - [Developers](#developers)
+  - [Thanks](#thanks)
+  - [Copyright and license](#copyright-and-license)
+
+## Quick start
+
+Download the latest version from our releases.
+
+- Run installer
+- Follow our step by step guide to setup
+- Shoot stuff!
+
+## Bugs and feature requests
+
+Have a bug or a feature request? Please first read the [issue guidelines](https://reponame/blob/master/CONTRIBUTING.md) and search for existing and closed issues. 
+
+If your problem or idea is not addressed yet, [please open a new issue](https://reponame/issues/new).
+
+## Contributing
+
+Feel free to fork this project. If you create something useful to the Lightgun community please create a pull request so we can all benefit.
+
+## Developers
+
+- [RetroTheArcadian](https://github.com/RetroTheArcadian)
+
+## Thanks
+
+Thank you is in order to all the people who have created builds with games and released them to the public.
+
+## Copyright and license
+
+Code and documentation copyright 2023-2024 the authors. Code released under the [MIT License](https://reponame/blob/master/LICENSE).
+
+Enjoy :metal:
