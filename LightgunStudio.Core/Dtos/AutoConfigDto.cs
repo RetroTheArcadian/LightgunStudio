a96c992f9149eeb0c7794c006ffb5d11218fa53f
RetroTheArcadian <retro.the.arcadian@gmail.com>
2025-01-06 09:33:29 +0100

    Update templates

---
+++ LightgunStudio.Core/Dtos/AutoConfigDto.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace LightgunStudio.Core.Dtos.AutoConfigDto
+{
+    public partial class AutoConfigDto
+    {
+        [JsonProperty("Lightguns", NullValueHandling = NullValueHandling.Ignore)]
+        public required List<Lightgun> Lightguns { get; set; }
+    }
+
+    public partial class Lightgun
+    {
+        [JsonProperty("HID", NullValueHandling = NullValueHandling.Ignore)]
+        public required string Hid { get; set; }
+
+        [JsonProperty("Name", NullValueHandling = NullValueHandling.Ignore)]
+        public required string Name { get; set; }
+    }
+}
---
+++ LightgunStudio.Core/Utilities/AutoConfig.cs
@@ -0,0 +1,32 @@
+﻿using LightgunStudio.Core.Dtos.AutoConfigDto;
+using Newtonsoft.Json;
+
+namespace LightgunStudio.Core.Utilities
+{
+    public class AutoConfig
+    {
+        public AutoConfigDto? GetAutoConfig()
+        {
+            var dllDirectory = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            if (dllDirectory == null) return null;
+            var root = Directory.GetParent(dllDirectory);
+            if (root == null) return null;
+            using (var r = new StreamReader(dllDirectory + @"\Templates\Config.json"))
+            {
+                string json = r.ReadToEnd();
+                if (json == null) return null;
+                return JsonConvert.DeserializeObject<AutoConfigDto>(json);
+            }
+        }
+        public List<Lightgun> GetActiveLightguns(List<string> hids)
+        {
+            var lightguns = new List<Lightgun>();
+            var cfg = GetAutoConfig();
+            foreach (var hid in hids) { 
+                var lightgun = cfg?.Lightguns.FirstOrDefault(x=> x.Hid.Contains(hid));
+                if(lightgun != null) {  lightguns.Add(lightgun); }
+            }
+            return lightguns;
+        }
+    }
+}
